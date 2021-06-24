b9a6769816ab3330776c25cf3df777539952036c
yeeseng98 <43286817+yeeseng98@users.noreply.github.com>
2021-06-24 16:00:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const client = new Discord.Client();
 admin.initializeApp({
     credential: admin.credential.cert({
         "projectId": process.env.FIREBASE_PROJECT_ID,
-        "private_key": process.env.FIREBASE_PRIVATE_KEY,
+        "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
         "client_email": process.env.FIREBASE_CLIENT_EMAIL
     }),
 });
